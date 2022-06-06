ee4a3129e9d0582978d8fc72ab71ab7fc4eeb278
Marvyn21 <marvynrogony@gmail.com>
2022-06-06 06:15:53 +0300

    first commit

---
+++ LoginPage(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginPage_MVC_.Models;
+
+namespace LoginPage_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginPage(MVC)/Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace LoginPage_MVC_.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
